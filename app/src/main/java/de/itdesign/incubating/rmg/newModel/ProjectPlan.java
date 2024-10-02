2b4cb45478946209dd73c3bcf4afca122646909f
kamalperisetti <perisettikamal@gmail.com>
2024-10-02 15:57:53 +0530

    Added Project and Project Plan

---
+++ app/src/main/java/de/itdesign/incubating/rmg/newModel/ProjectPlan.java
@@ -0,0 +1,65 @@
+package de.itdesign.incubating.rmg.newModel;
+
+
+import de.itdesign.incubating.rmg.model.Player;
+import de.itdesign.incubating.rmg.model.Project;
+import de.itdesign.incubating.rmg.model.ResourceCard;
+
+import java.util.Collection;
+
+public class ProjectPlan {
+    String id;
+    Player owner;
+    Project project;
+    int projectStartTime;
+    Collection<ResourceCard> cards;
+
+    public ProjectPlan(String id, Player owner, Project project, int projectStartTime, Collection<ResourceCard> cards) {
+        this.id = id;
+        this.owner = owner;
+        this.project = project;
+        this.projectStartTime = projectStartTime;
+        this.cards = cards;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public int getProjectStartTime() {
+        return projectStartTime;
+    }
+
+    public void setProjectStartTime(int projectStartTime) {
+        this.projectStartTime = projectStartTime;
+    }
+
+    public Collection<ResourceCard> getCards() {
+        return cards;
+    }
+
+    public void setCards(Collection<ResourceCard> cards) {
+        this.cards = cards;
+    }
+}
+
--- app/src/main/java/de/itdesign/incubating/rmg/service/GameService.java
+++ app/src/main/java/de/itdesign/incubating/rmg/service/GameService.java
@@ -1,16 +1,16 @@
-package de.itdesign.incubating.rmg.service;
-
-import de.itdesign.incubating.rmg.forworkingmodel.Game;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
-public class GameService {
-
-    Game newGame = new Game();
-
-    public Game getGame(){
-        return newGame;
-    }
-}
+//package de.itdesign.incubating.rmg.service;
+//
+//import de.itdesign.incubating.rmg.forworkingmodel.Game;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class GameService {
+//
+//    Game newGame = new Game();
+//
+//    public Game getGame(){
+//        return newGame;
+//    }
+//}
 
