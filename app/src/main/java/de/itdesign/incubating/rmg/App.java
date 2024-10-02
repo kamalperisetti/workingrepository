e2a53033df1eb46430958cff948bca3e130c1f7e
kamalperisetti <perisettikamal@gmail.com>
2024-10-02 14:41:10 +0530

    new model

--- app/src/main/java/de/itdesign/incubating/rmg/App.java
+++ app/src/main/java/de/itdesign/incubating/rmg/App.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        System.out.println("SLJKBHLKJQWJSLK");
+//        System.out.println("SLJKBHLKJQWJSLK");
+        System.out.println(new App().getGreeting());
     }
 }
