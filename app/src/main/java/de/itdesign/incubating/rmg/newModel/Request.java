f01717fc4af42154e4e97e7615bbbd5f6450597d
bhagyasribhagi <bhagyasribai0519@gmail.com>
2024-10-02 16:02:35 +0530

    RequestModel created1

---
+++ app/src/main/java/de/itdesign/incubating/rmg/newModel/Request.java
@@ -0,0 +1,40 @@
+package de.itdesign.incubating.rmg.newModel;
+
+import de.itdesign.incubating.rmg.model.Demand;
+
+public class Request {
+
+    String id;
+    String targetProjectBoardId;
+    Demand demand;
+
+    public Request(String id,String targetProjectBoardId,Demand demand){
+        this.id=id;
+        this.targetProjectBoardId=targetProjectBoardId;
+        this.demand=demand;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTargetProjectBoardId() {
+        return targetProjectBoardId;
+    }
+
+    public void setTargetProjectBoardId(String targetProjectBoardId) {
+        this.targetProjectBoardId = targetProjectBoardId;
+    }
+
+    public Demand getDemand() {
+        return demand;
+    }
+
+    public void setDemand(Demand demand) {
+        this.demand = demand;
+    }
+}
