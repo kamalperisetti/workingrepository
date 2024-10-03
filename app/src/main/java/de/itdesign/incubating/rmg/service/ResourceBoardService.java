ad8676b673be0248643c1ffa57aec1a6165631e6
gowtham <gowtham6008@gmail.com>
2024-10-03 11:25:20 +0530

    added resourceboardservice

---
+++ app/src/main/java/de/itdesign/incubating/rmg/service/ResourceBoardService.java
@@ -0,0 +1,10 @@
+package de.itdesign.incubating.rmg.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ResourceBoardService {
+    public  void Greet(){
+        System.out.println("Hello");
+    }
+}
